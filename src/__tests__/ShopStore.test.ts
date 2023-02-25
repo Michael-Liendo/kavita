2983fb6c745a53d0b763aca3cef16cff962e5c8f
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-02-25 09:21:22 -0400

    test: useCart test

---
+++ src/__tests__/ShopStore.test.ts
@@ -0,0 +1,83 @@
+import { Product } from '@/utils/types/products';
+import { act } from '@testing-library/react';
+import { useCart } from '../store/ShopStore';
+
+describe('useCart', () => {
+  const product1: Product = {
+    id: 1,
+    title: 'Product 1',
+    price: 10,
+    quantity: 0,
+    description: '',
+    images: [],
+    category: { id: 1, image: '', name: 'text' },
+  };
+  const product2: Product = {
+    id: 2,
+    title: 'Product 2',
+    price: 20,
+    quantity: 0,
+    description: '',
+    images: [],
+    category: { id: 1, image: '', name: 'text' },
+  };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should add a product to the cart', () => {
+    const { addProduct } = useCart.getState();
+
+    act(() => {
+      addProduct(product1);
+    });
+
+    const { cart } = useCart.getState();
+    expect(cart).toEqual([product1]);
+  });
+
+  it('should increment the quantity of an existing product', () => {
+    const { addProduct, decrementQuantity } = useCart.getState();
+
+    act(() => {
+      addProduct(product1);
+    });
+
+    act(() => {
+      addProduct(product1);
+    });
+
+    const { cart } = useCart.getState();
+    expect(cart).toEqual([{ ...product1, quantity: 2 }]);
+
+    act(() => {
+      decrementQuantity(product1);
+    });
+
+    const { cart: updatedCart } = useCart.getState();
+    expect(updatedCart).toEqual([{ ...product1, quantity: 1 }]);
+  });
+
+  it('should remove a product from the cart', () => {
+    const { addProduct, removeProduct } = useCart.getState();
+
+    act(() => {
+      addProduct(product1);
+    });
+
+    act(() => {
+      addProduct(product2);
+    });
+
+    const { cart } = useCart.getState();
+    expect(cart).toEqual([product1, product2]);
+
+    act(() => {
+      removeProduct(0);
+    });
+
+    const { cart: updatedCart } = useCart.getState();
+    expect(updatedCart).toEqual([product2]);
+  });
+});
