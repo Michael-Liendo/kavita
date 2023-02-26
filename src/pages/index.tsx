abc1fa57ed901ceb37e5191f52da63733c3d0ce9
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-02-25 20:52:15 -0400

    design: add more break points

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,73 +32,75 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main>
-        <div className="flex md:hidden justify-center mt-4">
-          <a href="#" className="w-[90vw] bg-slate-400 h-72">
-            {banners && (
-              <Image
-                className="rounded-lg w-full h-full object-cover"
-                src={banners?.head_main_banners?.banners_mobile[0] || ''}
-                alt="s"
-                width="1000"
-                height="1000"
-              />
-            )}
-          </a>
-        </div>
-        <div className="hidden md:flex w-screen flex-row justify-center mt-6 space-x-5">
-          <a href="#" className="bg-slate-400 w-[30%] h-53">
-            {banners && (
-              <Image
-                className="rounded-lg w-full h-full object-cover"
-                src={banners?.head_main_banners?.banners_computer[0] || ''}
-                alt="s"
-                width="1000"
-                height="1000"
-              />
-            )}
-          </a>
-          <div className="flex-col w-[40%] h-full">
-            <div className="w-full h-25">
-              <h3 className="mb-3 text-xl font-medium">Week selection</h3>
-              <div className="flex space-x-9">
-                {productWeekSelection?.map((product: Product) => (
-                  <a href="#" className="w-[22%]" key={product._id}>
-                    <Image
-                      className="rounded-md h-[75%] mb-2 object-cover"
-                      alt={product.description}
-                      src={product.images[0]}
-                      width={208}
-                      height={208}
-                    />
-                    <h2 className="truncate">{product.title}</h2>
-                  </a>
-                ))}
+      <main className="mx-8 lg:mx-28 xl:mx-40 2xl:mx-60">
+        <div>
+          <div className="flex md:hidden mt-4">
+            <a href="#" className="bg-slate-400 h-72">
+              {banners && (
+                <Image
+                  className="rounded-lg h-full object-cover"
+                  src={banners?.head_main_banners?.banners_mobile[0] || ''}
+                  alt="s"
+                  width="1000"
+                  height="1000"
+                />
+              )}
+            </a>
+          </div>
+          <div className="hidden md:flex flex-row mt-6 space-x-5">
+            <a href="#" className="bg-slate-400 min-w-[5rem] h-53">
+              {banners && (
+                <Image
+                  className="rounded-lg w-full h-full object-cover"
+                  src={banners?.head_main_banners?.banners_computer[0] || ''}
+                  alt="s"
+                  width="1000"
+                  height="1000"
+                />
+              )}
+            </a>
+            <div className="flex-col max-w-[50%] h-full">
+              <div className="w-full h-25">
+                <h3 className="mb-3 text-xl font-medium">Week selection</h3>
+                <div className="flex space-x-3 md:space-x-7 lg:space-x-5 xl:space-x-7">
+                  {productWeekSelection?.map((product: Product) => (
+                    <a href="#" className="w-[22%]" key={product._id}>
+                      <Image
+                        className="rounded-md h-[75%] mb-2 object-cover"
+                        alt={product.description}
+                        src={product.images[0]}
+                        width={208}
+                        height={208}
+                      />
+                      <h2 className="truncate">{product.title}</h2>
+                    </a>
+                  ))}
+                </div>
               </div>
-            </div>
-            <div className="hidden sm:flex h-72 mt-3">
-              <a href="#" className="mr-1 bg-slate-400 w-6/12">
-                {banners && (
-                  <Image
-                    className="rounded-lg w-full h-full object-cover"
-                    src={banners?.head_main_banners?.banners_computer[1] || ''}
-                    alt="s"
-                    width="1000"
-                    height="1000"
-                  />
-                )}
-              </a>
-              <a href="#" className="ml-1 bg-slate-400 w-6/12">
-                {banners && (
-                  <Image
-                    className="rounded-sm w-full h-full object-cover"
-                    src={banners?.head_main_banners?.banners_computer[2] || ''}
-                    alt="s"
-                    width="1000"
-                    height="1000"
-                  />
-                )}
-              </a>
+              <div className="hidden sm:flex h-72 mt-3">
+                <a href="#" className="mr-1 bg-slate-400 w-6/12">
+                  {banners && (
+                    <Image
+                      className="rounded-lg w-full h-full object-cover"
+                      src={banners?.head_main_banners?.banners_computer[1] || ''}
+                      alt="s"
+                      width="1000"
+                      height="1000"
+                    />
+                  )}
+                </a>
+                <a href="#" className="ml-1 bg-slate-400 w-6/12">
+                  {banners && (
+                    <Image
+                      className="rounded-sm w-full h-full object-cover"
+                      src={banners?.head_main_banners?.banners_computer[2] || ''}
+                      alt="s"
+                      width="1000"
+                      height="1000"
+                    />
+                  )}
+                </a>
+              </div>
             </div>
           </div>
         </div>
