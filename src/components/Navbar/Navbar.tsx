2674fadf10cc76f4e89277dc023ae1d1f2abca38
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-01 16:49:44 -0400

    style: navbar background color (#15)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import Menu from './Menu';
 
 export default function Navbar() {
   return (
-    <nav className="py-3.5 sm:py-5 bg-gradient-to-r from-blue-800 via-blue-700 to-blue-700 text-white flex justify-between px-4 sm:px-10 lg:px-28 xl:px-16 2xl:px-60 space-x-4">
+    <nav className="py-3.5 sm:py-5 bg-gradient-to-r from-[#0daac5] via-blue-[#0ea8c4] to-blue-600 text-white flex justify-between px-4 sm:px-10 lg:px-28 xl:px-16 2xl:px-60 space-x-4">
       <div className="flex items-center space-x-3.5 md:space-x-8">
         <Menu />
         <h1 className="text-2xl md:text-5xl">KAV</h1>
