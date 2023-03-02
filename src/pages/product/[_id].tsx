58c592a35bb56bfa7c68f7f0adfcd79f773634dd
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-02 11:12:46 -0400

    feat: product page

---
+++ src/pages/product/[_id].tsx
@@ -0,0 +1,241 @@
+import Layout from '@/components/Layout';
+import { getPublishedRandomProducts, getSingleProduct } from '@/lib/sanityFunctions';
+import { useCart } from '@/store/ShopStore';
+import { Product } from '@/utils/types/products';
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+export default function ProductPage({ product }: { product: Product }) {
+  const { addProduct } = useCart();
+  const [imageIndex, setImageIndex] = useState(0);
+  const [randomsProducts, setRandomsProducts] = useState<Product[] | []>();
+
+  useEffect(() => {
+    async function getRandomsProducts() {
+      const publishedProducts = await getPublishedRandomProducts(4);
+      setRandomsProducts(publishedProducts);
+    }
+    getRandomsProducts();
+  }, []);
+
+  return (
+    <Layout title={product.title}>
+      <div className=" my-8 container mx-auto px-4">
+        <nav className="flex">
+          <ol role="list" className="flex items-center">
+            <li className="text-left">
+              <div className="-m-1">
+                <Link
+                  href="/"
+                  className="rounded-md p-1 text-sm font-medium text-gray-600 focus:text-gray-900 focus:shadow hover:text-gray-800"
+                >
+                  Home
+                </Link>
+              </div>
+            </li>
+
+            <li className="text-left">
+              <div className="flex items-center">
+                <span className="mx-2 text-gray-400">/</span>
+                <div className="-m-1">
+                  <Link
+                    href="/product"
+                    className="rounded-md p-1 text-sm font-medium text-gray-600 focus:text-gray-900 focus:shadow hover:text-gray-800"
+                  >
+                    Products
+                  </Link>
+                </div>
+              </div>
+            </li>
+
+            <li className="text-left">
+              <div className="flex items-center">
+                <span className="mx-2 text-gray-400">/</span>
+                <div className="-m-1">
+                  <a
+                    href="#"
+                    title={product.title}
+                    className="rounded-md p-1 text-sm font-medium text-gray-600 focus:text-gray-900 focus:shadow hover:text-gray-800"
+                    aria-current="page"
+                  >
+                    {product.title.slice(0, 20)}
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ol>
+        </nav>
+
+        <div className="lg:col-gap-12 xl:col-gap-16 mt-8 grid grid-cols-1 gap-12 lg:mt-12 lg:grid-cols-5 lg:gap-16">
+          <div className="lg:col-span-3 lg:row-end-1">
+            <div className="lg:flex lg:items-start">
+              <div className="lg:order-2 lg:ml-5">
+                <div className="max-w-xl overflow-hidden rounded-lg">
+                  <Image
+                    className="h-full w-full max-w-full object-cover"
+                    src={product.images[imageIndex]}
+                    alt={product.title}
+                    width={600}
+                    height={600}
+                  />
+                </div>
+              </div>
+
+              <div className="mt-2 w-full lg:order-1 lg:w-32 lg:flex-shrink-0">
+                <div className="flex flex-row items-start lg:flex-col">
+                  {product.images.map((image, index) => (
+                    <button
+                      key={index}
+                      onClick={() => setImageIndex(index)}
+                      type="button"
+                      className="flex-0 aspect-square mb-3 h-20 overflow-hidden rounded-lg border-2 border-gray-900 text-center"
+                    >
+                      <Image
+                        className="h-full w-full object-cover"
+                        src={image}
+                        alt={product.title}
+                        height={600}
+                        width={600}
+                      />
+                    </button>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="lg:col-span-2 lg:row-span-2 lg:row-end-2">
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">{product.title}</h1>
+            <span className="text-gray-900">{product.category.name}</span>
+
+            <div className="mt-10 flex flex-col items-center justify-between space-y-4 border-t border-b py-4 sm:flex-row sm:space-y-0">
+              <div className="flex items-end">
+                <h1 className="text-3xl font-bold">${product.price}</h1>
+              </div>
+
+              <button
+                type="button"
+                onClick={() => addProduct(product)}
+                className="inline-flex items-center justify-center rounded-md border-2 border-transparent bg-gray-900 bg-none px-12 py-3 text-center text-base font-bold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-gray-800"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="shrink-0 mr-3 h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                  />
+                </svg>
+                Add to cart
+              </button>
+            </div>
+
+            <ul className="mt-8 space-y-2">
+              <li className="flex items-center text-left text-sm font-medium text-gray-600">
+                <svg
+                  className="mr-2 block h-5 w-5 align-middle text-gray-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    className=""
+                  ></path>
+                </svg>
+                Shipping worldwide
+              </li>
+            </ul>
+          </div>
+
+          <div className="lg:col-span-3">
+            <div className="border-b border-gray-300">
+              <nav className="flex gap-4">
+                <a
+                  title="Description"
+                  className="border-b-2 border-gray-900 py-4 text-sm font-medium text-gray-900 hover:border-gray-400 hover:text-gray-800"
+                >
+                  Description
+                </a>
+              </nav>
+            </div>
+
+            <div className="mt-8 flow-root sm:mt-12">{product.description}</div>
+          </div>
+        </div>
+      </div>
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Customers also purchased
+        </h2>
+
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {randomsProducts ? (
+            randomsProducts.map((product: Product, index) => (
+              <RandomProductCard key={index} product={product} />
+            ))
+          ) : (
+            <p>Loading</p>
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+function RandomProductCard({ product }: { product: Product }) {
+  return (
+    <Link href={`/product/${product._id}`} className="group relative">
+      <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+        <Image
+          src={product.images[0]}
+          alt="Front of men&#039;s Basic Tee in black."
+          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-sm text-gray-700">
+            <a href="#">
+              <span aria-hidden="true" className="absolute inset-0"></span>
+              {product.title}
+            </a>
+          </h3>
+          <p className="mt-1 text-sm text-gray-500"></p>
+        </div>
+        <p className="text-lg font-medium text-gray-900">${product.price}</p>
+      </div>
+    </Link>
+  );
+}
+
+export async function getServerSideProps({ params }: { params: Params }) {
+  const { _id } = params;
+
+  const product = await getSingleProduct(_id);
+  if (product) {
+    return {
+      props: {
+        product,
+      },
+    };
+  } else {
+    return {
+      notFound: true,
+    };
+  }
+}
