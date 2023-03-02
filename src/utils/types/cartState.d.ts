987e9021246cc6e177d717e2aeca6fdfd4a818fb
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-01 22:11:41 -0400

    chore: remove cart animated (#16)

--- src/utils/types/cartState.d.ts
+++ src/utils/types/cartState.d.ts
@@ -1,8 +1,6 @@
 export type CartState = {
   cart: Product[];
-  cartAnimated: boolean;
   addProduct: (product: Product) => void;
   removeProduct: (productIndex: number) => void;
   decrementQuantity: (product: Product) => void;
-  setCartAnimated: (value) => void;
 };
