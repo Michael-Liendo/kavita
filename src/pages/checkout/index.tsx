066fd5dc91232fe1f8614deb00c691a02635e657
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-03 12:15:24 -0400

    chore: remove shipping price

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -267,10 +267,6 @@ export default function Checkout() {
                 <p className="text-sm font-medium text-gray-900">Subtotal</p>
                 <p className="font-semibold text-gray-900">${cartTotal}</p>
               </div>
-              <div className="flex items-center justify-between">
-                <p className="text-sm font-medium text-gray-900">Shipping</p>
-                <p className="font-semibold text-gray-900">$8.00</p>
-              </div>
             </div>
             <div className="mt-6 flex items-center justify-between">
               <p className="text-sm font-medium text-gray-900">Total</p>
