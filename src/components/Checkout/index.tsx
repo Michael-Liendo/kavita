8c01f27160d33d3ceb0adaf330e731d94c086cb6
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-03 12:18:34 -0400

    feat: checkout page (#23)
    * feat: checkout page
    
    * chore: default checked
    
    * chore: remove shipping price

---
+++ src/components/Checkout/index.tsx
@@ -0,0 +1,21 @@
+import { Product } from '@/utils/types/products';
+import Image from 'next/image';
+
+export default function ProductCheckout({ product }: { product: Product }) {
+  return (
+    <div className="flex flex-col rounded-lg bg-white sm:flex-row">
+      <Image
+        className="m-2 h-24 w-28 rounded-md border object-cover object-center"
+        src={product.images[0]}
+        width={122}
+        height={96}
+        alt={product.title}
+      />
+      <div className="flex w-full flex-col px-4 py-4">
+        <span className="font-semibold">{product.title}</span>
+        <p className="text-lg font-bold">${product.price}</p>
+        <span>Quantity: {product.quantity}</span>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/checkout/index.tsx
@@ -0,0 +1,283 @@
+import Image from 'next/image';
+import ProductCheckout from '../../components/Checkout';
+import { useCart } from '../../store/ShopStore';
+import Layout from '@/components/Layout';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { Product } from '@/utils/types/products';
+
+export default function Checkout() {
+  const [cartState, setCartState] = useState<Product[]>([]);
+
+  const { cart } = useCart();
+
+  useEffect(() => {
+    setCartState(cart);
+  }, [cart]);
+
+  const cartTotal = cartState.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.price * currentValue.quantity,
+    0,
+  );
+
+  return (
+    <Layout title="Checkout | Kavita">
+      <nav className="flex mt-10">
+        <ol role="list" className="flex items-center">
+          <li className="text-left">
+            <div className="-m-1">
+              <Link
+                href="/"
+                className="rounded-md p-1 text-sm font-medium text-gray-600 focus:text-gray-900 focus:shadow hover:text-gray-800"
+              >
+                Home
+              </Link>
+            </div>
+          </li>
+
+          <li className="text-left">
+            <div className="flex items-center">
+              <span className="mx-2 text-gray-400">/</span>
+              <div className="-m-1">
+                <Link
+                  href="/checkout"
+                  className="rounded-md p-1 text-sm font-medium text-gray-600 focus:text-gray-900 focus:shadow hover:text-gray-800"
+                >
+                  Checkout
+                </Link>
+              </div>
+            </div>
+          </li>
+        </ol>
+      </nav>
+
+      <div className=" grid lg:grid-cols-2">
+        <div className="px-4 pt-8">
+          <p className="text-xl font-medium">Order Summary</p>
+          <p className="text-gray-400">Check your items. And select a suitable shipping method.</p>
+          <div className="mt-8 space-y-3 rounded-lg overflow-y-scroll max-h-96 border bg-white px-2 py-4 sm:px-6">
+            {cartState.length > 0 ? (
+              cartState.map((product) => <ProductCheckout product={product} key={product._id} />)
+            ) : (
+              <Link href="/">
+                <button
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                >
+                  Add products to the cartState
+                </button>
+              </Link>
+            )}
+          </div>
+
+          <p className="mt-8 text-lg font-medium">Shipping Methods</p>
+          <form className="mt-5 grid gap-6">
+            <div className="relative">
+              <input
+                className="peer hidden"
+                id="radio_1"
+                type="radio"
+                name="radio"
+                defaultChecked
+              />
+              <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+              <label
+                className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
+                htmlFor="radio_1"
+              >
+                <Image
+                  className="w-14 object-contain"
+                  src="https://graffica.info/wp-content/uploads/2017/06/FedEXCabEnfocado-1200x900.jpg"
+                  alt=""
+                  width={56}
+                  height={56}
+                />
+                <div className="ml-5">
+                  <span className="mt-2 font-semibold">Fedex Delivery</span>
+                  <p className="text-slate-500 text-sm leading-6">Delivery: 2-4 Days</p>
+                </div>
+              </label>
+            </div>
+            <div className="relative">
+              <input
+                className="peer hidden"
+                id="radio_2"
+                type="radio"
+                name="radio"
+                defaultChecked
+              />
+              <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+              <label
+                className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
+                htmlFor="radio_2"
+              >
+                <Image
+                  className="w-14 object-contain"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/United_Parcel_Service_logo_2014.svg/1200px-United_Parcel_Service_logo_2014.svg.png?w=144"
+                  alt=""
+                  width={56}
+                  height={56}
+                />
+                <div className="ml-5">
+                  <span className="mt-2 font-semibold">UPS</span>
+                  <p className="text-slate-500 text-sm leading-6">Delivery: 2-4 Days</p>
+                </div>
+              </label>
+            </div>
+          </form>
+        </div>
+        <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
+          <p className="text-xl font-medium">Payment Details</p>
+          <p className="text-gray-400">Complete your order by providing your payment details.</p>
+          <div className="">
+            <label htmlFor="email" className="mt-4 mb-2 block text-sm font-medium">
+              Email
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                id="email"
+                name="email"
+                className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="your.email@gmail.com"
+              />
+              <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  />
+                </svg>
+              </div>
+            </div>
+            <label htmlFor="card-holder" className="mt-4 mb-2 block text-sm font-medium">
+              Card Holder
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                id="card-holder"
+                name="card-holder"
+                className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="Your full name here"
+              />
+              <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
+                  />
+                </svg>
+              </div>
+            </div>
+            <label htmlFor="card-no" className="mt-4 mb-2 block text-sm font-medium">
+              Card Details
+            </label>
+            <div className="flex">
+              <div className="relative w-7/12 flex-shrink-0">
+                <input
+                  type="text"
+                  id="card-no"
+                  name="card-no"
+                  className="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                  placeholder="xxxx-xxxx-xxxx-xxxx"
+                />
+                <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                  <svg
+                    className="h-4 w-4 text-gray-400"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z" />
+                    <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm13 2v5H1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm-1 9H2a1 1 0 0 1-1-1v-1h14v1a1 1 0 0 1-1 1z" />
+                  </svg>
+                </div>
+              </div>
+              <input
+                type="text"
+                name="credit-expiry"
+                className="w-full rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="MM/YY"
+              />
+              <input
+                type="text"
+                name="credit-cvc"
+                className="w-1/6 flex-shrink-0 rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="CVC"
+              />
+            </div>
+            <label htmlFor="billing-address" className="mt-4 mb-2 block text-sm font-medium">
+              Billing Address
+            </label>
+            <div className="flex flex-col sm:flex-row">
+              <div className="relative flex-shrink-0 sm:w-7/12">
+                <input
+                  type="text"
+                  id="billing-address"
+                  name="billing-address"
+                  className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                  placeholder="Street Address"
+                />
+                <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                  <Image
+                    className="h-4 w-4 object-contain"
+                    src="https://flagpack.xyz/_nuxt/4c829b6c0131de7162790d2f897a90fd.svg"
+                    alt=""
+                    height={16}
+                    width={16}
+                  />
+                </div>
+              </div>
+              <select
+                name="billing-state"
+                className="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+              >
+                <option value="State">State</option>
+              </select>
+              <input
+                type="text"
+                name="billing-zip"
+                className="flex-shrink-0 rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none sm:w-1/6 focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="ZIP"
+              />
+            </div>
+
+            {/* <!-- Total --> */}
+            <div className="mt-6 border-t border-b py-2">
+              <div className="flex items-center justify-between">
+                <p className="text-sm font-medium text-gray-900">Subtotal</p>
+                <p className="font-semibold text-gray-900">${cartTotal}</p>
+              </div>
+            </div>
+            <div className="mt-6 flex items-center justify-between">
+              <p className="text-sm font-medium text-gray-900">Total</p>
+              <p className="text-2xl font-semibold text-gray-900">${cartTotal}</p>
+            </div>
+          </div>
+          <button className="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white">
+            Place Order
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
+}
