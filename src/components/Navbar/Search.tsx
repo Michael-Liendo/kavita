8e5ff7610a4dcc1688b48c890055f7de72f13490
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-03-08 11:05:54 -0400

    fix: better finder

--- src/components/Navbar/Search.tsx
+++ src/components/Navbar/Search.tsx
@@ -8,18 +8,8 @@ export default function Search() {
   const form = useRef<HTMLFormElement>(null);
 
   const [suggestIsOpen, setSuggestIsOpen] = useState(false);
-  const [findResult, setFindResult] = useState<Product[]>([]);
+  const [searchTerm, setSearchTem] = useState('');
   const [products, setProducts] = useState<Product[]>([]);
-
-  function onChangeHandler(e: any) {
-    setFindResult(() => {
-      if (e.target.value.length > 0)
-        return products.filter((products) =>
-          products.title.toLowerCase().includes(e.target.value.toLowerCase()),
-        );
-      return products.splice(0, 4);
-    });
-  }
 
   useEffect(() => {
     async function getProducts() {
@@ -47,7 +37,7 @@ export default function Search() {
   return (
     <form ref={form} className="flex relative md:ml-14">
       <input
-        onChange={onChangeHandler}
+        onChange={(event) => setSearchTem(event.target.value)}
         type="search"
         className="block w-full md:w-[30vw] py-1.5 px-1 md:py-3 md:px-4 text-sm placeholder-black text-gray-900 rounded-l-md bg-gray-50"
         placeholder="Busca productos..."
@@ -56,15 +46,18 @@ export default function Search() {
       {suggestIsOpen && (
         <div className="py-2 rounded px-3 absolute text-black bg-white w-full top-8 md:top-10">
           <ul className="truncate">
-            {findResult.length >= 1 ? (
-              findResult.map(({ title, _id }, index) => (
+            {products
+              .filter((product) => {
+                if (searchTerm == '') return product;
+                else if (product.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()))
+                  return product;
+              })
+              .splice(0, 5)
+              .map(({ title, _id }, index) => (
                 <li className="my-2" key={index} title={title}>
                   <Link href={`/product/${_id}`}>{title}</Link>
                 </li>
-              ))
-            ) : (
-              <li>Buscando productos...</li>
-            )}
+              ))}
           </ul>
         </div>
       )}
